2a99ad13e8d752f3e95321fabe0484d5e2390374
25138王科貿 <43640555+HelloHerba@users.noreply.github.com>
2019-02-21 19:25:11 +0800

    Update 寒假作業---回文判斷.cpp

--- 寒假作業---回文判斷.cpp
+++ 寒假作業---回文判斷.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 using namespace std;
 int main() {
-int num,i;
-string word;
-cout<<"請輸入一串文字：";
-cin>>word;
-num=word.length();
-for(int i=0;i<num;i++)
-{
-    cout<<word[i]<<"\n";
-}
-cout<<"此串文字是否為回文：";
-for(i = 0 ; i < num ; i++)
-{
-    if(word[i]==word[num-1-i])
+    int num,i;
+    string word;
+    cout<<"請輸入一串文字：";
+    cin>>word;
+    num=word.length();
+    for(int i=0;i<num;i++)
     {
-        continue;
+        cout<<word[i]<<"\n";
     }
-    else
+    cout<<"此串文字是否為回文：";
+    for(i = 0 ; i < num ; i++)
     {
-        cout<<"no";
-        return 0;
-    }    
-}
-if(i==num)
-{
-    cout<<"yes";
-}
+        if(word[i]==word[num-1-i])
+        {
+            continue;
+        }
+        else
+        {
+            cout<<"no";
+            return 0;
+        }    
+    }
+    if(i==num)
+    {
+        cout<<"yes";
+    }
 }
